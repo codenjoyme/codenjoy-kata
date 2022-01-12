dd13d8d4c3306616e91905ed2ea46815ba00acd3
Oleksandr Baglai <oleksandr_baglai@epam.com>
2022-01-12 23:08:23 +0200

    [engine][games][all][clients][java] Performance optimization near ElementsMap. Fixed rubicscube game.

--- src/test/java/com/codenjoy/dojo/kata/SmokeTest.java
+++ src/test/java/com/codenjoy/dojo/kata/SmokeTest.java
@@ -47,7 +47,7 @@ public class SmokeTest {
 
     @Test
     public void test() {
-        // about 6 sec
+        // about 5.0 sec
         int ticks = 1000;
 
         smoke.play(ticks, "SmokeTest.data",
